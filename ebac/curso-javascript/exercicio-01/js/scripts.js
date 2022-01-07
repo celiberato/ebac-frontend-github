971660d29d658f5461fcc672a1f4d7c2171b0336
Carlos Liberato <celiberato@hotmail.com>
2022-01-07 09:08:26 -0300

    modulo javascript

---
+++ ebac/curso-javascript/exercicio-01/js/scripts.js
@@ -0,0 +1,9 @@
+function obterMedia() {
+    let a = parseInt(document.getElementById("a").value);
+    let b = parseInt(document.getElementById("b").value);
+    let c = parseInt(document.getElementById("c").value);
+
+    let media = (a + b + c) / 3;
+
+    document.getElementById("resultado").innerHTML = "Resultado: " + media;
+}
